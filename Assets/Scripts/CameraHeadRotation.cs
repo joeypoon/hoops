9f7198e9ffc963b23d76e8e9325c738261133323
Joey F. Poon <joey@kaijudev.com>
2017-01-17 21:36:46 -0600

    Camera gimbal.

--- Assets/Scripts/CameraHeadRotation.cs
+++ Assets/Scripts/CameraHeadRotation.cs
@@ -2,21 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraRotation : MonoBehaviour {
+public class CameraHeadRotation : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
         RotateCamera();
-	}
+    }
 
     void RotateCamera()
     {
         float rotationSpeed = 2.0f;
-        float mouseX = Input.GetAxis("Mouse X") * rotationSpeed;
-        transform.rotation = transform.rotation * Quaternion.Euler(0, mouseX, 0);
+        float mouseY = Input.GetAxis("Mouse Y") * rotationSpeed;
+        transform.rotation = transform.rotation * Quaternion.Euler(-mouseY, 0, 0);
     }
 }
