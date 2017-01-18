980223b817656f64cd9ed4734a904d4778e8443d
Joey F. Poon <joey@kaijudev.com>
2017-01-17 20:48:44 -0600

    Spawn ball with velocity.

--- Assets/Scripts/BallSpawner.cs
+++ Assets/Scripts/BallSpawner.cs
@@ -8,11 +8,29 @@ public class BallSpawner : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        Instantiate(ballPrefab);
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        ShootBall(KeyCode.LeftArrow);
+        ShootBall(KeyCode.RightArrow);
+        ShootBall(KeyCode.UpArrow);
+        ShootBall(KeyCode.DownArrow);
+    }
+
+    void ShootBall(UnityEngine.KeyCode key)
+    {
+        int velocityMultiplier = 5;
+        if (Input.GetKeyDown(key))
+        {
+            SpawnBall().velocity = Vector3.left * velocityMultiplier;
+        }
+    }
+
+    Rigidbody SpawnBall()
+    {
+        return Instantiate(ballPrefab)
+            .GetComponent<Rigidbody>();
+    }
 }
