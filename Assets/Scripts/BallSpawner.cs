274986b8da8edf2994167d9392a3a463f37522ab
Joey F. Poon <joey@kaijudev.com>
2017-01-17 20:19:50 -0600

    Generate ball on start.

---
+++ Assets/Scripts/BallSpawner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSpawner : MonoBehaviour {
+
+    public GameObject ballPrefab;
+
+	// Use this for initialization
+	void Start () {
+        Instantiate(ballPrefab);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
