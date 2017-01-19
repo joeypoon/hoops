1de48bb144081b911ebcba889bbd02cdcb9448e8
Joey F. Poon <joey@kaijudev.com>
2017-01-18 20:52:38 -0600

    Basic target collision and score keeping.

---
+++ Assets/Scripts/ScoreKeeper.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreKeeper : MonoBehaviour {
+
+    public int score = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void incrementScore()
+    {
+        score++;
+        print("Your score is: " + score);
+    }
+}
