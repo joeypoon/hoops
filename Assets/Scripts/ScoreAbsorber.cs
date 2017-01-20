ac83d297eb5427b41026559940d26c06b120e46c
Joey F. Poon <joey@kaijudev.com>
2017-01-19 20:38:24 -0600

    Add score to game over screen.

---
+++ Assets/Scripts/ScoreAbsorber.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreAbsorber : MonoBehaviour {
+
+    public int score = 0;
+    private ScoreKeeper scoreKeeper;
+    private Text scoreDisplay;
+
+	// Use this for initialization
+	void Start () {
+        scoreKeeper = FindObjectOfType<ScoreKeeper>();
+        if (scoreKeeper)
+        {
+            score = scoreKeeper.score;
+            Destroy(scoreKeeper.gameObject);
+        }
+
+        scoreDisplay = GetComponent<Text>();
+        scoreDisplay.text = "Score: " + score;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Scripts/ScoreKeeper.cs
+++ Assets/Scripts/ScoreKeeper.cs
@@ -8,7 +8,7 @@ public class ScoreKeeper : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+        DontDestroyOnLoad(gameObject);
 	}
 	
 	// Update is called once per frame
